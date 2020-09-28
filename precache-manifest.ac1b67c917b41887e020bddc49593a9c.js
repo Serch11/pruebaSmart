dec47aa8846ea362ba44a4825706d19afb4da0d8
Serch <sredondo95@gmail.com>
2020-09-28 12:43:26 -0500

    Updates

---
+++ precache-manifest.ac1b67c917b41887e020bddc49593a9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c072d80407519cb4a105148f6c7886",
+    "url": "/pruebaSmart/index.html"
+  },
+  {
+    "revision": "53b9a84eef79633b976c",
+    "url": "/pruebaSmart/static/css/2.2d63d0b4.chunk.css"
+  },
+  {
+    "revision": "3ede0844533f1200108a",
+    "url": "/pruebaSmart/static/css/main.2dd1f77c.chunk.css"
+  },
+  {
+    "revision": "53b9a84eef79633b976c",
+    "url": "/pruebaSmart/static/js/2.1ff11a3f.chunk.js"
+  },
+  {
+    "revision": "bfac3db2904ab62270a9981733ca061a",
+    "url": "/pruebaSmart/static/js/2.1ff11a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ede0844533f1200108a",
+    "url": "/pruebaSmart/static/js/main.1e416be3.chunk.js"
+  },
+  {
+    "revision": "1fdec9e542bb0a05dc4c",
+    "url": "/pruebaSmart/static/js/runtime-main.f7e48458.js"
+  }
+]);
